e37b6a787db1159f235aeb6f53bf95f2a4951880
noorteck-sdet <119881887+noorteck-sdet@users.noreply.github.com>
2023-07-30 12:25:10 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -1,18 +1,5 @@
 package demo;
 
 public class DemoDay3 {
-
-	public static void main(String[] args) {
-		System.out.println("Demo..");
-		
-		System.out.println("Khwaja Sediqi");
-		
-		System.out.println("This is my second practice");
-		
-		System.out.println("Sammy");
-		
-		String country = "USA";
-		
-		System.out.println(country);
-	}
+//importance of following stash process && communicating... :)
 }
